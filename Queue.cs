4e0d8cacca26f6e1a0adcecae53f1844d703ecda
Your Name <you@example.com>
2022-08-13 22:45:39 +0530

    [Megha] Add: Created Queue

---
+++ Queue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class Queue
+    {
+        public static void InsertQueue()
+        {
+            Console.WriteLine("Insert data using queue");
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+
+            foreach (int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("After Dequue ");
+
+
+        }
+    }
+}
