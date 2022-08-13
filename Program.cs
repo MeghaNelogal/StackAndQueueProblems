3a74419df9c231b1cee57bb895573b38f8f24616
Your Name <you@example.com>
2022-08-13 22:46:02 +0530

    [Megha] Add: Added UC3

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@
         {
             Console.WriteLine("***Welcome to Stack and Queue Problems***");
 
-            Stack.CreateStack();
+            Console.WriteLine("Please Enter the Information");
+            bool check = true;
+            Console.WriteLine("1.To Display Stack \n 2.To Display Queue \n");
+
+            while (check)
+            {
+                Console.WriteLine("Enter The Above option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Stack.CreateStack();
+                        break;
+                    case 2:
+
+                        Queue.InsertQueue();
+                        break;
+
+                    default:
+                        check = false;
+                        break;
+                }
+
+            }
+
         }
     }
 }
+   
